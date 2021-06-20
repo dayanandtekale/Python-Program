ba6a0df3d93670483cce06be47af56b4a21e21c6
Dayanand Shivraj Tekale <81641502+dayanandtekale@users.noreply.github.com>
2021-06-20 12:33:12 +0530

    Add files via upload

---
+++ star_pattern_with_nested_for.py
@@ -0,0 +1,9 @@
+# for num in range(6):
+#     for num2 in range(num):
+#         print("*", end='')
+#     print("")
+
+a = [30, 54, 67, 12, 89, 45, {'a':[400, 600, 800], 'b':[700, 900, 100]}] 
+for dict_a in a[6]:
+    for i in a[6][dict_a]:
+        print(i)
