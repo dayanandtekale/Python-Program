cc41f138547d145267c904a14742e62718a58cc8
Dayanand Shivraj Tekale <81641502+dayanandtekale@users.noreply.github.com>
2021-06-20 12:32:24 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,38 @@
+num = -10
+while(num<10):
+    if num%2==0:
+        print("even number "+ str(num))
+    else:
+        print("odd number "+ str(num))
+    num = num + 1
+
+# print("last number ", num)
+
+# import time
+# num = 10
+# end = num * 10
+# while(num<=end):
+#     print(num, end)
+#     time.sleep(1)
+#     num = num + 10
+
+
+# a = [30, 54, 67, 12, 89, 45]
+# i = 0  # initialization / value assignment
+# length = len(a)
+# while(i < length): # Condition Check
+#     print(a[i])
+#     i += 1 # stride / step
+
+# a = {'A':30, 'B':54, 'C':67, 'D':12, 'E':89, 'F':45}
+
+# b = list(a.keys())
+# i = 0
+# length = len(b)
+# while(i < length):
+#     print(b[i] ,a[b[i]])
+#     i = i + 1
+
+# b[0] = 'A'
+# b[1] = 'B'
+# b[2] = 'C'
